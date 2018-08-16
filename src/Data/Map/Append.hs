1b3f038b9adc7e08332885a0e92193c9678cb788
Alexey Kotlyarov <a@koterpillar.com>
2018-08-16 20:47:18 +1000

    Do not require Monoid on elements

--- src/Data/Map/Append.hs
+++ src/Data/Map/Append.hs
@@ -3,10 +3,10 @@ module Data.Map.Append where
 import qualified Data.Map as Map
 import Data.Map (Map)
 
-import qualified Data.List.NonEmpty as NE
-
 import Data.Monoid
 import Data.Semigroup
+
+import qualified Data.List.NonEmpty as NE
 
 newtype AppendMap k v = AppendMap
   { unAppendMap :: Map k v
@@ -16,6 +16,6 @@ instance (Ord k, Semigroup v) => Semigroup (AppendMap k v) where
   AppendMap a <> AppendMap b = AppendMap $ Map.unionWith (<>) a b
   sconcat = AppendMap . Map.unionsWith (<>) . NE.toList . fmap unAppendMap
 
-instance (Ord k, Monoid v) => Monoid (AppendMap k v) where
+instance (Ord k, Semigroup v) => Monoid (AppendMap k v) where
   mempty = AppendMap Map.empty
   mconcat = AppendMap . Map.unionsWith (<>) . fmap unAppendMap
